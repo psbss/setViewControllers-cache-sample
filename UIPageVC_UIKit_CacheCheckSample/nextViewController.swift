b602a139be25154d985f6865071305b0ae9b3221
uetyo <uehara.yuki@dely.jp>
2021-04-26 18:53:41 +0900

    working

--- UIPageVC_UIKit_CacheCheckSample/nextViewController.swift
+++ UIPageVC_UIKit_CacheCheckSample/nextViewController.swift
@@ -9,23 +9,26 @@ import UIKit
 
 class PageViewController: UIPageViewController, UIPageViewControllerDataSource {
     var reloadBarBtnItem: UIBarItem!
+    private var centerIndex: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.dataSource = self
-        setVC()
         reloadBarBtnItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(reloadBarBtnTapped(_:)))
         self.navigationItem.rightBarButtonItem = (reloadBarBtnItem as! UIBarButtonItem)
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        setVC()
+    }
     private lazy var pages: [UIViewController] = {
         return [
-            createPage(color: .yellow),
+            createPage(color: .red),
             createPage(color: .blue),
-            createPage(color: .gray)
+            createPage(color: .yellow),
         ]
     }()
-    
     private func randomColor() -> UIColor {
         var arr: [UIColor]
         arr = [
@@ -38,53 +41,37 @@ class PageViewController: UIPageViewController, UIPageViewControllerDataSource {
         ]
         return arr.randomElement()!
     }
-
+    // animated: true の場合、2枚目に遷移後1枚目に戻り、リロードすると2枚目の画面が変化していない→キャッシュされている。
+    // animated: false の場合、キャッシュされないので画面は変更される。
     private func setVC() {
-        self.setViewControllers([pages[0]], direction: .forward, animated: true, completion: nil)
+        self.setViewControllers([pages[centerIndex]], direction: .forward, animated: true, completion: nil)
     }
-
     private func createPage(color: UIColor) -> UIViewController {
         let page = UIViewController()
         page.view.backgroundColor = color
-        let btn: UIButton = UIButton(frame: CGRect(x: 0, y: 50, width: self.view.frame.width/2, height: self.view.frame.height/4))
-        btn.backgroundColor = .white
-        btn.setTitle("Btn", for: .normal)
-        btn.setTitleColor(.black, for: .normal)
-        btn.addTarget(self, action: #selector(PageViewController.nextBtn(_:)), for: .touchUpInside)
-        self.view.addSubview(btn)
         return page
     }
-
-    @objc func nextBtn(_ sender: UIButton) {
-        print("HQ")
-        let vc = ViewController()
-        self.navigationController?.pushViewController(vc, animated: true)
-    }
-
     @objc func reloadBarBtnTapped(_ sender: UIBarButtonItem) {
-        pages[2] = createPage(color: .green)
+        pages[centerIndex + 1] = createPage(color: .green)
         setVC()
         print("page reloaded")
     }
-
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
         if let pageIndex = pages.firstIndex(of: viewController), pageIndex - 1 >= 0 {
             return pages[pageIndex - 1]
         }
         return nil
     }
-
+    // 以下にブレイクポイントを張ってみるとキャッシュ時の動作が分かりやすい
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
         if let pageIndex = pages.firstIndex(of: viewController), pageIndex + 1 < pages.count {
             return pages[pageIndex + 1]
         }
         return nil
     }
-
     func presentationCount(for pageViewController: UIPageViewController) -> Int {
         return pages.count
     }
-
     func presentationIndex(for pageViewController: UIPageViewController) -> Int {
         return 0
     }
